e711470df017fd39c1831b7580d578df88e2b358
jonahgabriel <jonah.gabriel@gmail.com>
2026-03-15 16:55:23 -0400

    fix: update _FALLBACK_MATRIX to match new dependency pins [OMN-5099]

--- src/omnibase_infra/runtime/version_compatibility.py
+++ src/omnibase_infra/runtime/version_compatibility.py
@@ -167,13 +167,13 @@ def _build_matrix_from_pyproject(
 _FALLBACK_MATRIX: list[VersionConstraint] = [
     VersionConstraint(
         package="omnibase_core",
-        min_version="0.27.0",
+        min_version="0.27.1",
         max_version="0.28.0",
     ),
     VersionConstraint(
         package="omnibase_spi",
-        min_version="0.16.1",
-        max_version="0.17.0",
+        min_version="0.17.0",
+        max_version="0.18.0",
     ),
 ]
 
