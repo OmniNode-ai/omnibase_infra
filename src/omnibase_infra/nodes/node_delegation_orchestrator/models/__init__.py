c75cdb8db1f143a1b34bef93e9b5b8e78e7c9311
jonahgabriel <jonah.gabriel@gmail.com>
2026-03-30 20:14:05 -0400

    feat: add delegation orchestrator node with FSM state machine [OMN-7040]

--- src/omnibase_infra/nodes/node_delegation_orchestrator/models/__init__.py
+++ src/omnibase_infra/nodes/node_delegation_orchestrator/models/__init__.py
@@ -4,14 +4,34 @@
 # Copyright (c) 2026 OmniNode Team
 """Models for the delegation orchestrator node."""
 
+from omnibase_infra.nodes.node_delegation_orchestrator.models.model_delegation_event import (
+    ModelDelegationEvent,
+)
 from omnibase_infra.nodes.node_delegation_orchestrator.models.model_delegation_request import (
     ModelDelegationRequest,
 )
 from omnibase_infra.nodes.node_delegation_orchestrator.models.model_delegation_result import (
     ModelDelegationResult,
 )
+from omnibase_infra.nodes.node_delegation_orchestrator.models.model_inference_intent import (
+    ModelInferenceIntent,
+)
+from omnibase_infra.nodes.node_delegation_orchestrator.models.model_inference_response_data import (
+    ModelInferenceResponseData,
+)
+from omnibase_infra.nodes.node_delegation_orchestrator.models.model_quality_gate_intent import (
+    ModelQualityGateIntent,
+)
+from omnibase_infra.nodes.node_delegation_orchestrator.models.model_routing_intent import (
+    ModelRoutingIntent,
+)
 
 __all__: list[str] = [
     "ModelDelegationRequest",
     "ModelDelegationResult",
+    "ModelInferenceResponseData",
+    "ModelDelegationEvent",
+    "ModelInferenceIntent",
+    "ModelQualityGateIntent",
+    "ModelRoutingIntent",
 ]
